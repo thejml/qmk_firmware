bb05cc8f6129907c9568fb5e80c50bab20b44dbb
Joe Legeckis <thejml@gmail.com>
2020-05-31 00:12:45 -0400

    Updated keymap for handwired dactyl manuform.

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/thejml/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/thejml/keymap.c
@@ -49,33 +49,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-    KC_ESC,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,   \
-    KC_TAB,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
-    KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
-                    KC_LBRC,KC_RBRC,                                            KC_PLUS,KC_EQL,                    \
-                                    RAISE,  KC_SPC,             KC_ENT, LOWER,                                     \
-                                    KC_TAB, KC_HOME,            KC_END, KC_DEL,                                    \
-                                    KC_BSPC,KC_GRV,             KC_LGUI,KC_LALT                                    \
+    KC_TAB,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSPC, \
+    KC_ESC,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,  \
+    KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RCTL,  \
+                    KC_LBRC,KC_RBRC,                                            KC_MINS,KC_EQL,                    \
+                            LOWER,  KC_ENT,                             KC_SPC, RAISE,                                     \
+                                    KC_LGUI,KC_LALT,            KC_RALT,KC_PIPE,                                    \
+                                    KC_BSPC,KC_GRV,             KC_RGUI,KC_BSLASH                                    \
 ),
 
 [_LOWER] = LAYOUT(
-    _______,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7, KC_P8,  KC_P9,  RESET,  KC_PLUS,   \
-    _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,            KC_RPRN, KC_P4, KC_P5,  KC_P6,  KC_MINS,KC_PIPE,   \
-    _______,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_UNDS,   \
-                    _______,KC_PSCR,                                           _______, KC_P0,                     \
-                    _______,_______,                                           _______,_______,                    \
-                                            _______,_______,    _______,_______,                                   \
-                                            _______,_______,    _______,_______                                    \
+    KC_GRV ,KC_MPLY,KC_PGDN, KC_UP, KC_PGUP,KC_VOLU,            KC_RBRC,  KC_7,  KC_8,   KC_9,  RESET,  KC_PLUS,   \
+    BL_INC ,KC_MFFD,KC_LEFT,KC_DOWN,KC_RGHT,KC_VOLD,            KC_RPRN,  KC_4,  KC_5,   KC_6,  KC_MINS,KC_PIPE,   \
+    BL_DEC ,KC_MRWD,KC_HOME,_______,KC_END, KC_MUTE,            _______,  KC_1,  KC_2,   KC_3,  KC_EQL, KC_UNDS,   \
+                    BL_TOGG,KC_PSCR,                                             KC_0, KC_ENT,                     \
+                            _______,_______,                            _______,_______,                    \
+                                    _______,_______,            _______,_______,                                   \
+                                    _______,_______,            _______,_______                                    \
 ),
 
 [_RAISE] = LAYOUT(
-    _______,RESET,  _______,_______,_______,KC_LBRC,            KC_RBRC,_______,KC_NLCK,KC_INS, KC_SLCK,KC_MUTE,   \
-    _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,            KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,   \
-    _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
-                                            _______,_______,    KC_EQL, _______,                                   \
-                                            _______,_______,    _______,_______,                                   \
-                                            _______,_______,    _______,_______,                                   \
-                                            _______,_______,    _______,_______                                    \
+    KC_F1  ,  KC_F2, KC_F3 ,  KC_F4,  KC_F5,  KC_F6,              KC_F7,  KC_F8,  KC_F9,KC_F10,  KC_F11, KC_F12,
+    BL_INC ,KC_LEFT, KC_UP ,KC_DOWN,KC_RGHT,KC_LPRN,            KC_RPRN,SCMD(KC_LEFT), SCMD(KC_RGHT),KC_MNXT,_______,KC_VOLU,   \
+    BL_DEC ,_______,_______,_______,_______,_______,            _______,RCTL(KC_LEFT), RCTL(KC_RGHT),_______,_______,KC_VOLD,   \
+                     BL_ON ,_______,                                    KC_EQL, _______,                                   \
+                            _______,_______,                            _______,_______,                                   \
+                                    _______,_______,            _______,_______,                                   \
+                                    _______,_______,            _______,_______                                    \
 )
 };
 
